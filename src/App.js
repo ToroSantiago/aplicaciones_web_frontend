bdf64e7c5c5e64525fbdf8dbfdf7b74c634d82ef
a256b <arielb02965@gmail.com>
2026-05-25 19:36:55 -0300

    feat: notificaciones

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './Layout/layout';
 import { registerServiceWorker } from './serviceWorkerManager';
+import { useDiscountNotifications } from './hooks/useDiscountNotifications';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   useEffect(() => {
-    // Registrar Service Worker para PWA + caché
     const initPWA = async () => {
       await registerServiceWorker();
     };
 
     initPWA();
   }, []);
+
+  // Hook para notificaciones de descuentos nuevos
+  useDiscountNotifications();
 
   return (
     <BrowserRouter>
